444a41620b7cbc109e7bec951e5d9010052b3ea7
magdielG <mcdielgmz@hotmail.com>
2020-12-17 12:34:50 -0700

    Add text

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import "./css/index.css";
-
+import text from './js/navbar'
 const app = document.getElementById("app");
 const div = document.createElement("div");
 
 div.innerHTML = `
 <div class="container">
 <p>Hola Mundo</p>
+${text}
 </div>
 `;
 app.appendChild(div);
